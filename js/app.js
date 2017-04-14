fb3342e83f3bb8bc41d701230e57701f08e71b8b
Simon Qi <smilexx2@gmail.com>
2017-04-14 18:19:38 +1000

    Side menu finished

--- js/app.js
+++ js/app.js
@@ -14,3 +14,17 @@ $.ajax({
 var getAvatarImageUrl = (user) => user.picture.thumbnail;
 
 var getName = (user) => user.name.first + ' ' + user.name.last;
+
+$('.hamburger-icon').click(function() {
+    $('aside').toggleClass('open');
+});
+
+function openNav() {
+    document.querySelector('aside').style.width = "70px";
+    document.querySelector('.main-content').style.marginLeft = "70px";
+}
+
+function closeNav() {
+    document.querySelector('aside').style.width = "0";
+    document.querySelector('.main-content').style.marginLeft = "0";
+}

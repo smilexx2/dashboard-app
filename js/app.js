7b8e912d1bf771b0d5efcc68b772ea6a4f8ce1cc
Simon Qi <smilexx2@gmail.com>
2017-04-22 15:57:16 +1000

    4 different scatter chart completed

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@ const avatarImg = document.querySelector('header .avatar');
 const nameLink = document.querySelector('header .name');
 const membersList = document.querySelector('.new-members .list');
 const recentActivitiesList = document.querySelector('.recent-activities .list');
+const timeScaleListItems = document.querySelectorAll('.scales li');
 
 const chevronIcon = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve" width="16"><g><g><polygon points="17.4,63.5 14.6,60.5 45.1,32 14.6,3.5 17.4,0.5 50.9,32"/></g></g></svg>';
 
 const $window = $(window);
 
 $.ajax({
-    url: 'https://randomuser.me/api/?results=500',
+    url: 'https://randomuser.me/api/?results=500&nat=us,ca,gb,au',
     dataType: 'json',
     success: function(data) {
         const loginUser = data.results[0];
@@ -136,17 +137,18 @@ $('.notification-icon').click(function() {
         $('.no-notification-message').addClass('show');
     }
 
+    if (!$('.dropdown').hasClass('hide')) {
+        $('body').addClass('disable-scroll');
+    }
+
     if ($window.width() < 768) {
         $('aside').removeClass('open');
-        if (!$('.dropdown').hasClass('hide')) {
-            $('body').addClass('disable-scroll');
-        }
     }
 
 });
 
 $(document).mouseup(function(e) {
-    var container = $('.dropdown, .notification-icon');
+    let container = $('.dropdown, .notification-icon');
 
     if (!container.is(e.target) && container.has(e.target).length === 0) {
         $('.dropdown').addClass('hide');
@@ -154,11 +156,202 @@ $(document).mouseup(function(e) {
     }
 });
 
-const endDate = moment().year(2017).month(6).date(31);
-const startDate = moment().year(2017).month(6).date(31).subtract(11, 'weeks').add(1, 'd');
+$('.scales li').click(function() {
+    $('.chart-wrapper.active').removeClass('active');
+    $('.scales li.active').removeClass('active');
+    $('.chart-wrapper').eq($('.scales li').index($(this))).addClass('active');
+    $(this).addClass('active');
+});
 
-let scatterChartWeekly = Chart.Scatter("scatterChartWeekly", {
-    data: {
+let dataHourly = [{
+    x: moment().year(2017).month(6).date(30).hour(0),
+    y: 12
+}, {
+    x: moment().year(2017).month(6).date(30).hour(1),
+    y: 18
+}];
+
+let optionsHourly = {
+    max: 25,
+    fixedStepSize: 5,
+    autoSkip: true,
+    time: {
+        unit: 'hour',
+        round: 'hour',
+        min: moment().year(2017).month(6).date(30).hour(0).minute(0),
+        max: moment().year(2017).month(6).date(31).hour(0).minute(0),
+        displayFormats: {
+            'millisecond': 'H',
+            'second': 'H',
+            'minute': 'H',
+            'hour': 'H',
+            'day': 'H',
+            'week': 'H',
+            'month': 'H',
+            'quarter': 'H',
+            'year': 'H'
+        },
+        tooltipFormat: 'H:00'
+    }
+}
+
+let dataDaily = [{
+    x: moment().year(2017).month(6).date(24),
+    y: 0
+}, {
+    x: moment().year(2017).month(6).date(25),
+    y: 175
+}, {
+    x: moment().year(2017).month(6).date(26),
+    y: 75
+}, {
+    x: moment().year(2017).month(6).date(30),
+    y: 175
+}, {
+    x: moment().year(2017).month(6).date(31),
+    y: 75
+}];
+
+let optionsDaily = {
+    max: 250,
+    fixedStepSize: 50,
+    autoSkip: false,
+    time: {
+        unit: 'day',
+        round: 'day',
+        min: moment().year(2017).month(6).date(31).subtract(7, 'd').hour(0).minute(0),
+        max: moment().year(2017).month(6).date(31).hour(0).minute(0),
+        displayFormats: {
+            'millisecond': 'ddd',
+            'second': 'ddd',
+            'minute': 'ddd',
+            'hour': 'ddd',
+            'day': 'ddd',
+            'week': 'ddd',
+            'month': 'ddd',
+            'quarter': 'ddd',
+            'year': 'ddd'
+        },
+        tooltipFormat: 'dddd'
+    }
+}
+
+let dataWeekly = [{
+        x: moment().year(2017).month(4).date(16),
+        y: 0
+    },
+    {
+        x: moment().year(2017).month(4).date(23),
+        y: 500
+    },
+    {
+        x: moment().year(2017).month(4).date(30),
+        y: 1000
+    },
+    {
+        x: moment().year(2017).month(5).date(2),
+        y: 750
+    },
+    {
+        x: moment().year(2017).month(5).date(9),
+        y: 1250
+    },
+    {
+        x: moment().year(2017).month(5).date(16),
+        y: 1750
+    },
+    {
+        x: moment().year(2017).month(5).date(23),
+        y: 1250
+    },
+    {
+        x: moment().year(2017).month(5).date(27),
+        y: 1500
+    },
+    {
+        x: moment().year(2017).month(6).date(4),
+        y: 1000
+    },
+    {
+        x: moment().year(2017).month(6).date(11),
+        y: 1500
+    },
+    {
+        x: moment().year(2017).month(6).date(18),
+        y: 2000
+    },
+    {
+        x: moment().year(2017).month(6).date(25),
+        y: 1500
+    },
+    {
+        x: moment().year(2017).month(7).date(1),
+        y: 2000
+    }
+];
+
+let optionsWeekly = {
+    max: 2500,
+    fixedStepSize: 500,
+    autoSkip: false,
+    time: {
+        round: 'day',
+        min: moment().year(2017).month(6).date(31).subtract(11, 'weeks').add(1, 'd'),
+        max: moment().year(2017).month(6).date(31),
+        unitStepSize: 7,
+        displayFormats: {
+            'millisecond': 'DD',
+            'second': 'DD',
+            'minute': 'DD',
+            'hour': 'DD',
+            'day': 'DD',
+            'week': 'DD',
+            'month': 'DD',
+            'quarter': 'DD',
+            'year': 'DD'
+        },
+        tooltipFormat: 'D-MMM-YYYY'
+
+    }
+}
+
+let dataMonthly = [{
+        x: moment().year(2017).month(0).date(0),
+        y: 5000
+    },
+    {
+        x: moment().year(2017).month(1),
+        y: 7500
+    }
+];
+
+let optionsMonthly = {
+    max: 25000,
+    fixedStepSize: 5000,
+    autoSkip: false,
+    time: {
+        unit: 'month',
+        round: 'month',
+        min: moment().year(2017).month(0).date(0),
+        max: moment().year(2018).month(0).date(1),
+        displayFormats: {
+            'millisecond': 'M',
+            'second': 'M',
+            'minute': 'M',
+            'hour': 'M',
+            'day': 'M',
+            'week': 'M',
+            'month': 'M',
+            'quarter': 'M',
+            'year': 'M'
+        },
+        tooltipFormat: 'MMMM YYYY'
+
+    }
+};
+
+let datasetsScatterChart = (data) => {
+    return {
         datasets: [{
             pointBackgroundColor: 'rgba(255, 255, 255, 1)',
             pointBorderColor: 'rgba(128, 134, 195, 1)',
@@ -167,62 +360,13 @@ let scatterChartWeekly = Chart.Scatter("scatterChartWeekly", {
             borderWidth: 2,
             lineTension: 0,
             backgroundColor: 'rgba(128, 134, 195, 0.3)',
-            data: [{
-                    x: moment().year(2017).month(4).date(16),
-                    y: 0
-                },
-                {
-                    x: moment().year(2017).month(4).date(23),
-                    y: 500
-                },
-                {
-                    x: moment().year(2017).month(4).date(30),
-                    y: 1000
-                },
-                {
-                    x: moment().year(2017).month(5).date(2),
-                    y: 750
-                },
-                {
-                    x: moment().year(2017).month(5).date(9),
-                    y: 1250
-                },
-                {
-                    x: moment().year(2017).month(5).date(16),
-                    y: 1750
-                },
-                {
-                    x: moment().year(2017).month(5).date(23),
-                    y: 1250
-                },
-                {
-                    x: moment().year(2017).month(5).date(27),
-                    y: 1500
-                },
-                {
-                    x: moment().year(2017).month(6).date(4),
-                    y: 1000
-                },
-                {
-                    x: moment().year(2017).month(6).date(11),
-                    y: 1500
-                },
-                {
-                    x: moment().year(2017).month(6).date(18),
-                    y: 2000
-                },
-                {
-                    x: moment().year(2017).month(6).date(25),
-                    y: 1500
-                },
-                {
-                    x: moment().year(2017).month(6).date(31),
-                    y: 2000
-                }
-            ]
+            data: data
         }]
-    },
-    options: {
+    }
+}
+
+let optionsScatterChart = (options) => {
+    return {
         legend: {
             display: false
         },
@@ -230,8 +374,8 @@ let scatterChartWeekly = Chart.Scatter("scatterChartWeekly", {
             yAxes: [{
                 ticks: {
                     beginAtZero: true,
-                    max: 2500,
-                    fixedStepSize: 500,
+                    max: options.max,
+                    fixedStepSize: options.fixedStepSize,
                     fontFamily: "'Roboto', sans-serif",
                     fontSize: 10,
                     fontColor: "#cacaca",
@@ -247,35 +391,44 @@ let scatterChartWeekly = Chart.Scatter("scatterChartWeekly", {
                 ticks: {
                     fontFamily: "'Roboto', sans-serif",
                     fontSize: 10,
-                    fontColor: "#cacaca"
+                    fontColor: "#cacaca",
+                    autoSkip: options.autoSkip,
+                    autoSkipPadding: 20
                 },
                 gridLines: {
                     zeroLineColor: "rgba(0, 0, 0, 0.1)"
                 },
                 type: 'time',
-                unit: 'week',
-                time: {
-                    min: startDate,
-                    max: endDate,
-                    unitStepSize: 7,
-                    displayFormats: {
-                        'millisecond': 'DD',
-                        'second': 'DD',
-                        'minute': 'DD',
-                        'hour': 'DD',
-                        'day': 'DD',
-                        'week': 'DD',
-                        'month': 'DD',
-                        'quarter': 'MDD',
-                        'year': 'DD'
-                    },
-                    tooltipFormat: 'D-MMM-YYYY'
-                }
+                time: options.time
             }]
         },
         responsive: true,
         maintainAspectRatio: false
     }
+}
+
+let scatterChartHourly = new Chart("scatterChartHourly", {
+    type: 'line',
+    data: datasetsScatterChart(dataHourly),
+    options: optionsScatterChart(optionsHourly)
+});
+
+let scatterChartDaily = new Chart("scatterChartDaily", {
+    type: 'line',
+    data: datasetsScatterChart(dataDaily),
+    options: optionsScatterChart(optionsDaily)
+})
+
+let scatterChartWeekly = new Chart("scatterChartWeekly", {
+    type: 'line',
+    data: datasetsScatterChart(dataWeekly),
+    options: optionsScatterChart(optionsWeekly)
+});
+
+let scatterChartMonthly = new Chart("scatterChartMonthly", {
+    type: 'line',
+    data: datasetsScatterChart(dataMonthly),
+    options: optionsScatterChart(optionsMonthly)
 });
 
 let barChartDaily = new Chart("barChartDaily", {
